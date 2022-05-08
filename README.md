de082e82c095e642457dacbad8b96bc6a6e89723
Sebastian Rendon Arteaga <serendona@unal.edu.co>
2022-05-08 12:07:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# movies_app
+
+A new Flutter application to see a lot of movies information.
---
+++ android/app/src/main/kotlin/Rendon/Corp/movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package Rendon.Corp.movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
