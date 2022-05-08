0ceb826ed1e4274e44edaa89b8cbf538f4fdbc2e
Sebastian Rendon Arteaga <serendona@unal.edu.co>
2022-05-08 17:09:52 -0500

    MyApp Const property issue fix
    Signed-off-by: Sebastian Rendon Arteaga <serendona@unal.edu.co>

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,14 @@ class AppState extends StatelessWidget {
           lazy: false,
         )
       ],
-      child: MyApp(),
+      child: const MyApp(),
     );
   }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
